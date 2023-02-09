7c187539f8cc5fb81aa9469626f7f37b9972620d
Luca <monteleone546@gmail.com>
2023-02-09 10:59:25 -0300

    Probando

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,10 @@
+package holamundo;
+/**
+ * 
+ * @author Luca Monte
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }  
+}
